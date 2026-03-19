fa538e56c1b4c9e6bccfd04ea255f3b1e346a6c2
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2026-03-19 12:04:59 -0500

    Miscellaneous changes

--- PreserveGeneticHair/SimHairGrowth.cs
+++ PreserveGeneticHair/SimHairGrowth.cs
@@ -1,31 +1,11 @@
-﻿using Sims3.Gameplay.CAS;
+﻿using System.Collections.Generic;
+using Sims3.Gameplay.CAS;
 
 namespace Destrospean.PreserveGeneticHair
 {
     public static class SimHairGrowth
     {
-        public static HairGrowthStates GetHairGrowthState(this SimDescription simDescription)
-        {
-            HairGrowthStates hairGrowthState;
-            return SimHairData.HairGrowthStates.TryGetValue(simDescription.SimDescriptionId, out hairGrowthState) ? hairGrowthState : SimHairData.HairGrowthStates[simDescription.SimDescriptionId] = 0;
-        }
-
-        public static bool LowerHairGrowthState(this SimDescription simDescription, int by = 1)
-        {
-            if (by < 1)
-            {
-                return false;
-            }
-            int newHairGrowthState = (int)simDescription.GetHairGrowthState() - by;
-            if (newHairGrowthState > 0)
-            {
-                simDescription.SetHairGrowthState((HairGrowthStates)newHairGrowthState);
-                return true;
-            }
-            return false;
-        }
-
-        public static bool RaiseHairGrowthState(this SimDescription simDescription, int by = 1)
+        public static bool DecrementHairGrowthState(this SimDescription simDescription, int by = 1)
         {
             if (by < 1)
             {
@@ -34,7 +14,28 @@ namespace Destrospean.PreserveGeneticHair
             int newHairGrowthState = (int)simDescription.GetHairGrowthState() + by;
             if (newHairGrowthState < System.Enum.GetValues(typeof(HairGrowthStates)).Length)
             {
-                simDescription.SetHairGrowthState((HairGrowthStates)newHairGrowthState);
+                simDescription.SetHairGrowthState(newHairGrowthState);
+                return true;
+            }
+            return false;
+        }
+
+        public static HairGrowthStates GetHairGrowthState(this SimDescription simDescription)
+        {
+            HairGrowthStates hairGrowthState;
+            return SimHairData.HairGrowthStates.TryGetValue(simDescription.SimDescriptionId, out hairGrowthState) ? hairGrowthState : SimHairData.HairGrowthStates[simDescription.SimDescriptionId] = 0;
+        }
+
+        public static bool IncrementHairGrowthState(this SimDescription simDescription, int by = 1)
+        {
+            if (by < 1)
+            {
+                return false;
+            }
+            int newHairGrowthState = (int)simDescription.GetHairGrowthState() - by;
+            if (newHairGrowthState > 0)
+            {
+                simDescription.SetHairGrowthState(newHairGrowthState);
                 return true;
             }
             return false;
@@ -43,6 +44,11 @@ namespace Destrospean.PreserveGeneticHair
         public static void SetHairGrowthState(this SimDescription simDescription, HairGrowthStates hairGrowthState)
         {
             SimHairData.HairGrowthStates[simDescription.SimDescriptionId] = hairGrowthState;
+        }
+
+        public static void SetHairGrowthState(this SimDescription simDescription, int hairGrowthState)
+        {
+            simDescription.SetHairGrowthState((HairGrowthStates)hairGrowthState);
         }
     }
 }
